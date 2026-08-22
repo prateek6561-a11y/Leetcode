dbd8246e0f2604554b6654a431a9de5daa31b16a
prateek6561-a11y <prateek6561@gmail.com>
2026-08-22 23:56:01 +0530

    Time: 1 ms (77.55%), Space: 46.8 MB (24.67%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,12 +1,11 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int k=1;
+        int k=0;
         for(int j=1;j<nums.length;j++){
             if(nums[j]!=nums[j-1]){
-                nums[k]=nums[j];
-                k=k+1;
+                nums[++k]=nums[j];
             }
         }
-        return k;
+        return ++k;
     }
 }
