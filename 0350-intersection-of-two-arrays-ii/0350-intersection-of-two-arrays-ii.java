dd73476d1d871547cb2bcc92af80a43cb101b49e
prateek6561-a11y <prateek6561@gmail.com>
2026-08-23 02:06:32 +0530

    Time: 6 ms (33.65%), Space: 45.1 MB (36.29%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,19 +1,38 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        Map<Integer,Integer> map=new HashMap<>();
-        for(int n:nums1){
-            map.put(n,map.getOrDefault(n,0)+1);
-        }
+        // Map<Integer,Integer> map=new HashMap<>();
+        // for(int n:nums1){
+        //     map.put(n,map.getOrDefault(n,0)+1);
+        // }
+        // int res[]=new int[nums1.length];
+        // int k=0;
+        // for(int n:nums2){
+        //     int count=map.getOrDefault(n,0);
+        //     if(count==0){
+        //         continue;
+        //     }else{
+        //         res[k]=n;
+        //         k++;
+        //         map.put(n,count-1);
+        //     }
+        // }
+        // return Arrays.copyOfRange(res,0,k);
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
         int res[]=new int[nums1.length];
         int k=0;
-        for(int n:nums2){
-            int count=map.getOrDefault(n,0);
-            if(count==0){
-                continue;
-            }else{
-                res[k]=n;
+        int i=0;
+        int j=0;
+        while(i< nums1.length && j<nums2.length){
+            if(nums1[i]==nums2[j]){
+                res[k]=nums1[i];
                 k++;
-                map.put(n,count-1);
+                i++;
+                j++;
+            }else if(nums1[i]>nums2[j]){
+j++;
+            }else{
+                i++;
             }
         }
         return Arrays.copyOfRange(res,0,k);
